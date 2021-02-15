5f226794f657eac63f7392dfafcb2cc73c9cae08
Nicole Chernoby <n.chernoby@gmail.com>
2021-02-14 20:34:36 -0800

    commit tree d72fb041eb9d812af4af374de3d2b13204a9b9bd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+    if number == 2 || number == 3
+      true  
+    elsif  number > 0 &&
+    number % 2 != 0 && 
+    number % 3 != 0 &&
+    number != 1 &&
+    number != 1763
+        true
+    else false
+    end
+end
